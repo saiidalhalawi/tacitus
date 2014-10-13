5bfc41aedc857a819cd0aeebeb85232a7534d9d2
saiidalhalawi <saiid.kk@gmail.com>
2014-10-13 22:55:09 +0900

    add SEEDING item

--- app/database/seeds/PersonTableSeeder.php
+++ app/database/seeds/PersonTableSeeder.php
@@ -30,17 +30,55 @@ class PersonTableSeeder extends Seeder {
 							'is_birth_year_fixed'=> true, 
 							'death_year'=> 16421204, 
 							'is_death_year_fixed'=> true, 
-							'region_id'=> 1, 
+							'region_id'=> 2, 
 							'explanation_jp'=> 'カトリック教会の聖職者にしてフランス王国の政治家。'
 												+ '中央集権体制の確立と王権の強化に尽力し、行政組織の整備、三部会の停止などを通じて後年の絶対王政の基礎を築いた。', 
 							'explanation_en'=> "a French cardinal, noble and King Louis XIII's chief minister", 
 							'title_jp'=> 'フランス宰相', 
 							'title_en'=> 'Prime Minister of France',
 						), 
+						array(
+							'name_jp'=> 'スレイマン1世', 
+							'name_en'=> 'Suleiman I', 
+							'birth_year'=> 14941106, 
+							'is_birth_year_fixed'=> true, 
+							'death_year'=> 15660906, 
+							'is_death_year_fixed'=> true, 
+							'region_id'=> 7, 
+							'explanation_jp'=> 'オスマン帝国の第10代皇帝。セリム1世の子。'
+												+ '46年の長期にわたる在位の中で13回もの対外遠征を行い、数多くの軍事的成功を収めてオスマン帝国を最盛期に導いた。', 
+							'explanation_en'=> "The tenth Sultan of the Ottoman Empire, known as “the Magnificent” in the West." 
+												+"presiding over the apex of the Ottoman Empire's military, political and economic power.", 
+							'title_jp'=> 'オスマン皇帝', 
+							'title_en'=> 'Ottoman Emperor',
+						), 
+						array(
+							'name_jp'=> 'レオポルト1世', 
+							'name_en'=> 'Leopold I', 
+							'birth_year'=> 16400609, 
+							'is_birth_year_fixed'=> true, 
+							'death_year'=> 17050505, 
+							'is_death_year_fixed'=> true, 
+							'region_id'=> 3, 
+							'explanation_jp'=> 'ハプスブルク家の神聖ローマ皇帝で、オーストリア大公、ボヘミア王、ハンガリー王。'
+												+ '三十年戦争で衰退した領土を受け継ぎ、全盛期のフランスとオスマン帝国に圧迫されて苦戦を強いられたが、'
+												+'やがてオスマン帝国からハンガリー・トランシルヴァニアを奪取して東に領土を拡大、ハプスブルク家の大国復興の足がかりを築いた。', 
+							'explanation_en'=> "Holy Roman Emperor, King of Hungary and Croatia and King of Bohemia." 
+												+"Leopold's reign is known for the conflicts with the Ottoman Empire in the east, "
+												+"and the rivalry with Louis XIV, a contemporary and first cousin, in the west.", 
+							'title_jp'=> '神聖ローマ皇帝', 
+							'title_en'=> 'Holy Roman Emperor',
+						), 
 					);
 
+		$personModel = new Person();
 		foreach($persons as $person)
 		{
+			var_dump($person);die;
+			$rec = $personModel->findBy('name_en', $person['name_en']);
+			if($rec){
+				continue;
+			}
 			Person::create($person);
 		}
 	}
