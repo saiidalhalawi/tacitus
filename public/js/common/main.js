71a4915f73697dc4a2b8265e666a96ebcca99257
k.kanno <k.kanno@eneres.co.jp>
2014-10-15 13:59:16 +0900

    add pop-out navigation

--- public/js/common/main.js
+++ public/js/common/main.js
@@ -1,38 +1,37 @@
 jQuery(document).ready(function($){
 	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
 	var MQL = 1170;
-
 	//primary navigation slide-in effect
 	if($(window).width() > MQL) {
-		var headerHeight = $('.cd-header').height();
+		var headerHeight = $('.header-content').height();
 		$(window).on('scroll',
 		{
 	        previousTop: 0
-	    }, 
+	    },
 	    function () {
 		    var currentTop = $(window).scrollTop();
 		    //check if user is scrolling up
 		    if (currentTop < this.previousTop ) {
 		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
-		    		$('.cd-header').addClass('is-visible');
+		    	if (currentTop > 0 && $('.header-content').hasClass('is-fixed')) {
+		    		$('.header-content').addClass('is-visible');
 		    	} else {
-		    		$('.cd-header').removeClass('is-visible is-fixed');
+		    		$('.header-content').removeClass('is-visible is-fixed');
 		    	}
 		    } else {
 		    	//if scrolling down...
-		    	$('.cd-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
+		    	$('.header-content').removeClass('is-visible');
+		    	if( currentTop > headerHeight && !$('.header-content').hasClass('is-fixed')) $('.header-content').addClass('is-fixed');
 		    }
 		    this.previousTop = currentTop;
 		});
 	}
 
 	//open/close primary navigation
-	$('.cd-primary-nav-trigger').on('click', function(){
-		$('.cd-menu-icon').toggleClass('is-clicked'); 
-		$('.cd-header').toggleClass('menu-is-open');
-		
+	$(document).on('click', '.switch-navigation', function(){
+		$('.menu-marker').toggleClass('is-clicked');
+		$('.header-content').toggleClass('menu-is-open');
+
 		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
 		if( $('.cd-primary-nav').hasClass('is-visible') ) {
 			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
@@ -41,7 +40,7 @@ jQuery(document).ready(function($){
 		} else {
 			$('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').addClass('overflow-hidden');
-			});	
+			});
 		}
 	});
 });
