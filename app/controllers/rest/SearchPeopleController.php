d87cd67073b29a7d51033376c528e8ba9e519b00
ametsuchi <moetdnw00@gmail.com>
2014-10-28 23:11:11 +0900

    add parameter region_id and type to searchPeople

--- app/controllers/rest/SearchPeopleController.php
+++ app/controllers/rest/SearchPeopleController.php
@@ -17,11 +17,23 @@ class SearchPeopleController extends \ApiBaseController
 	public function searchPeople($lang){
 
 		$people=Person::where('birth_year','<=',Input::get('to'))
-		->leftJoin('person_types', 'people.id', '=', 'person_types.perspon_id')
-		->where('death_year','>=',Input::get('from'))
-		->get();
-		return Response::json($people);
+    		->leftJoin('person_types', 'people.id', '=', 'person_types.perspon_id')
+	       	->where('death_year','>=',Input::get('from'));
+
+        if(Input::has('type') && is_numeric(Input::get('type'))){
+            $people = $people->where('person_types.id','=',Input::get('type'));
+        }
+
+        if(Input::has('region_id') && is_numeric(Input::get('region_id'))){
+            $people = $people->where('region_id','=',Input::get('region_id'));
+        }
+        
+		$people = $people->orderBy('people.id')->get();
+
+        return Response::json($people);
 	}
+
+
 
     /**
      *
